a35c3ad3c5f59d37da8ed73dfcabf48ddbf3c667
Matthew Lu <pamalu.225@gmail.com>
2026-05-12 14:58:52 -0400

    training pipeline cdk

---
+++ training/cdk/bin/cpak-infra.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { CpakStack } from '../lib/cpak-stack';
+
+const app = new cdk.App();
+
+new CpakStack(app, 'CpakInfraStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION || 'us-east-1',
+  },
+  description: 'CPAK SageMaker training pipeline infrastructure',
+});
